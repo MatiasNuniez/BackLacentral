4754aab56e81818dc635e44359bed96dd706bc0d
matiasnuniez <matiasnuniez192@gmail.com>
2024-02-15 00:44:51 -0300

    akshdasd

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ class Server extends Database {
     
     this.app.use(express.json())
 
-    this.app.use(cors({credentials: true, origin: 'https://front-lacentral.vercel.app:3000/login'}))
+    this.app.use(cors({credentials: true, origin: 'https://front-lacentral.vercel.app/'}))
 
     this.app.use(cookiesParser())
     
