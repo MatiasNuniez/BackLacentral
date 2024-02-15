29c59694e06e5f7aeff1faeb33cf717d7e7f8de1
matiasnuniez <matiasnuniez192@gmail.com>
2024-02-14 21:43:19 -0300

    origin

--- src/index.ts
+++ src/index.ts
@@ -19,7 +19,7 @@ class Server extends Database {
     
     this.app.use(express.json())
 
-    this.app.use(cors({credentials: true, origin: 'https://backlacentral.onrender.com/api'}))
+    this.app.use(cors({credentials: true, origin: 'http://backlacentral.onrender.com'}))
 
     this.app.use(cookiesParser())
     
