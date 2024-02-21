2919ee731d5df83d9c62185b94eb39295987a82f
MatiasNuniez <matiasnuniez192@gmail.com>
2024-02-21 05:27:59 -0300

    asdasd

--- src/controllers/general.controller.ts
+++ src/controllers/general.controller.ts
@@ -6,56 +6,58 @@ export class GeneralController extends AuthMiddleware {
 
   async getElements(req: Request, res: Response) {
     const user = Array.isArray(req.headers['user'])
-    ? req.headers['user'][0] 
-    : req.headers['user'] || '';
+      ? req.headers['user'][0]
+      : req.headers['user'] || '';
     const token = req.headers.authorization?.split(' ')[1]
-      try {
-        await modelGeneral.find({ state: true }).then((data) => {
-          res.send(data)
-        })
-      } catch (error) {
-        res.send({ ERROR: error })
-      }
+    try {
+      await modelGeneral.find({ state: true }).then((data) => {
+        res.send(data)
+      })
+    } catch (error) {
+      res.send({ ERROR: error })
     }
+  }
 
   async insertElement(req: Request, res: Response) {
     const user = Array.isArray(req.headers['user'])
-    ? req.headers['user'][0] 
-    : req.headers['user'] || '';
+      ? req.headers['user'][0]
+      : req.headers['user'] || '';
     const token = req.headers.authorization?.split(' ')[1]
-      try {
-        const data = req.body
-        await modelGeneral.insertMany(data)
-      } catch (error) {
-        res.send({ ERROR: error })
-      }
+    try {
+      const data = req.body
+      await modelGeneral.insertMany(data)
+      const lastElement = await modelGeneral.find({ user: { $eq: data.numero } })
+      res.send(lastElement)
+    } catch (error) {
+      res.send({ ERROR: error })
     }
+  }
 
   async editElement(req: Request, res: Response) {
     const user = Array.isArray(req.headers['user'])
-    ? req.headers['user'][0] 
-    : req.headers['user'] || '';
+      ? req.headers['user'][0]
+      : req.headers['user'] || '';
     const token = req.headers.authorization?.split(' ')[1]
-      try {
-        const { id } = req.params
-        const update = req.body
-        await modelGeneral.updateOne({ _id: id }, update)
-      } catch (error) {
-        res.send({ ERROR: error })
-      }
+    try {
+      const { id } = req.params
+      const update = req.body
+      await modelGeneral.updateOne({ _id: id }, update)
+    } catch (error) {
+      res.send({ ERROR: error })
     }
-    
+  }
+
 
   async deleteElement(req: Request, res: Response) {
     const user = Array.isArray(req.headers['user'])
-    ? req.headers['user'][0] 
-    : req.headers['user'] || '';
+      ? req.headers['user'][0]
+      : req.headers['user'] || '';
     const token = req.headers.authorization?.split(' ')[1]
-      try {
-        const { id } = req.params
-        await modelGeneral.updateOne({ _id: id }, { $set: { state: false } })
-      } catch (error) {
-        res.send({ ERROR: error })
-      }
-    } 
+    try {
+      const { id } = req.params
+      await modelGeneral.updateOne({ _id: id }, { $set: { state: false } })
+    } catch (error) {
+      res.send({ ERROR: error })
     }
+  }
+}
