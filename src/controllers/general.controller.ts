f647ba318219ecb987ec84955c119e1f1597ae7e
MatiasNuniez <matiasnuniez192@gmail.com>
2024-02-21 02:24:57 -0300

    post-delete-put

--- src/controllers/general.controller.ts
+++ src/controllers/general.controller.ts
@@ -5,11 +5,10 @@ import { AuthMiddleware } from "../middlewares/auth.middleware";
 export class GeneralController extends AuthMiddleware {
 
   async getElements(req: Request, res: Response) {
-    const user = Array.isArray(req.headers['user']) 
+    const user = Array.isArray(req.headers['user'])
     ? req.headers['user'][0] 
     : req.headers['user'] || '';
     const token = req.headers.authorization?.split(' ')[1]
-    console.log(token);
       try {
         await modelGeneral.find({ state: true }).then((data) => {
           res.send(data)
@@ -20,10 +19,10 @@ export class GeneralController extends AuthMiddleware {
     }
 
   async insertElement(req: Request, res: Response) {
-    const user = Array.isArray(req.headers['user']) 
+    const user = Array.isArray(req.headers['user'])
     ? req.headers['user'][0] 
-    : req.headers['user'] || ''
-    const token = req.headers.authorization?.split('')[1] || '';
+    : req.headers['user'] || '';
+    const token = req.headers.authorization?.split(' ')[1]
       try {
         const data = req.body
         await modelGeneral.insertMany(data)
@@ -33,10 +32,10 @@ export class GeneralController extends AuthMiddleware {
     }
 
   async editElement(req: Request, res: Response) {
-    const user = Array.isArray(req.headers['user']) 
+    const user = Array.isArray(req.headers['user'])
     ? req.headers['user'][0] 
-    : req.headers['user'] || ''
-    const token = req.headers.authorization?.split('')[1] || '';
+    : req.headers['user'] || '';
+    const token = req.headers.authorization?.split(' ')[1]
       try {
         const { id } = req.params
         const update = req.body
@@ -48,10 +47,10 @@ export class GeneralController extends AuthMiddleware {
     
 
   async deleteElement(req: Request, res: Response) {
-    const user = Array.isArray(req.headers['user']) 
+    const user = Array.isArray(req.headers['user'])
     ? req.headers['user'][0] 
-    : req.headers['user'] || ''
-    const token = req.headers.authorization?.split('')[1] || '';
+    : req.headers['user'] || '';
+    const token = req.headers.authorization?.split(' ')[1]
       try {
         const { id } = req.params
         await modelGeneral.updateOne({ _id: id }, { $set: { state: false } })
